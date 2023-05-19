be77ce9f97c38338ad9d752c4a9597d17667de9d
Cristobal Cañas <103609041+Ccanas-h@users.noreply.github.com>
2023-05-19 18:27:47 -0400

    Update EmailValidatorTest.java

--- src/test/java/email2/EmailValidatorTest.java
+++ src/test/java/email2/EmailValidatorTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
 import org.junit.Test;
 
